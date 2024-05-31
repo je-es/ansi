0822a3220cf50df661eafa98840f636ecf9cd289
Maysara <Maysara.Elshewehy@gmail.com>
2024-05-31 08:03:50 +0300

    cleanup

--- src/code/modules/style.ts
+++ src/code/modules/style.ts
@@ -40,11 +40,11 @@
     /**
      * Applies styles to a given string.
      *
-     * @param {string} str - The string to apply styles to.
-     * @param {i_style} options - The styles to apply.
-     * @param {string|t_rgb} options.fg - The foreground color style to apply.
-     * @param {string|t_rgb} options.bg - The background color style to apply.
-     * @param {string|string[]} options.attr - The attribute style to apply.
+     * @param {string}  str             - The string to apply styles to.
+     * @param {i_style} options         - The styles to apply.
+     * @param {t_color} options.fg      - The foreground color style to apply.
+     * @param {t_color} options.bg      - The background color style to apply.
+     * @param {t_attr}  options.attr    - The attribute style to apply.
      *
      * @return {string} The string with styles applied.
     */
@@ -116,14 +116,14 @@
         /**
          * Applies a color style to the foreground or background of a string.
          *
-         * @param {string | t_rgb} ref - The color reference to apply. Can be a string representing a color name or a hex code, or an array representing RGB values.
+         * @param {t_color} ref - The color reference to apply. Can be a string representing a color name or a hex code, or an array representing RGB values.
          * @param {string} [_for='fg'] - The type of color to apply. Must be 'fg' for foreground or 'bg' for background. Defaults to 'fg'.
          * @return {string} The ANSI escape code representing the color style.
          * @throws {Error} If _for is not 'fg' or 'bg', or if ref is not a non-empty string or array[n,n,n].
          * @throws {Error} If ref is a string and not a valid color name or hex code, or if ref is an array and not of length 3.
          * @throws {Error} If ref is an array and any of the RGB values are not numbers between 0 and 255.
         */
-        color: (ref: string | t_rgb, _for: string = 'fg')
+        color: (ref: t_color, _for: string = 'fg')
         : string =>
         {
             const validColorTypes = ['fg', 'bg'];
