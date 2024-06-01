faf8d8a2b16fd26a9c94edea57de0c7cfb44d1c7
Maysara <Maysara.Elshewehy@gmail.com>
2024-06-01 16:46:30 +0300

    Update style.ts

--- src/code/modules/style.ts
+++ src/code/modules/style.ts
@@ -118,73 +118,67 @@
         /**
          * Applies a color style to the foreground or background of a string.
          *
-         * @param {t_color} ref             - The color reference to apply. Can be a string representing a color name or a hex code, or an array representing RGB values.
-         * @param {string}  [_for='fg']     - The type of color to apply. Must be 'fg' for foreground or 'bg' for background. Defaults to 'fg'.
+         * @param { t_color }       ref     - The color reference to apply. Can be a string representing a color name or a hex code, or an array representing RGB values.
+         * @param { 'fg' | 'bg' }   _for    - The type of color to apply. Must be 'fg' for foreground or 'bg' for background.
          *
-         * @throws {Error} If _for is not 'fg' or 'bg', or if ref is not a non-empty string or array[n,n,n].
-         * @throws {Error} If ref is a string and not a valid color name or hex code, or if ref is an array and not of length 3.
+         * @throws {Error} If ref is a string and not a valid color name or hex code.
          * @throws {Error} If ref is an array and any of the RGB values are not numbers between 0 and 255.
          *
          * @return {string} The ANSI escape code representing the color style.
         */
-        color: (ref: t_color, _for: string = 'fg')
+        color: (ref: t_color, _for: 'fg' | 'bg')
         : string =>
         {
-            const validColorTypes = ['fg', 'bg'];
-
-            if (!validColorTypes.includes(_for))
-            {
-                throw new Error("_for must be 'fg' or 'bg'");
-            }
-
+            // by string
             if (typeof ref === 'string')
             {
+                // by string : name
                 if (codes[_for as keyof typeof codes].hasOwnProperty(ref))
                 {
                     return (codes[_for as keyof typeof codes] as { [key: string]: string })[ref];
                 }
 
-                const hexRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
-
-                if (hexRegex.test(ref))
-                    {
+                // by string : hex
+                if (/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(ref))
+                {
+                    // #? -> ?
                     let hex = ref.replace('#', '');
 
-                    if (hex.length === 3) {
-                        hex = hex.split('').map(char => char + char).join('');
-                    }
+                    // 3 -> 6
+                    if (hex.length === 3)
+                    hex = hex.split('').map(char => char + char).join('');
 
+                    // 6 -> bigint
                     const bigint = parseInt(hex, 16);
+
+                    // bigint -> rgb
                     const r = (bigint >> 16) & 255;
                     const g = (bigint >> 8) & 255;
                     const b = bigint & 255;
+
+                    // rgb -> string
                     return _for === 'fg' ? `\u001b[38;2;${r};${g};${b}m` : `\u001b[48;2;${r};${g};${b}m`;
                 }
 
-                else
-                {
-                    throw new Error('Invalid hex color code');
-                }
-
+                // invalid string syntax
+                throw new Error('Invalid string color syntax');
             }
 
-            else if (Array.isArray(ref) && ref.length === 3)
-            {
-                const [r, g, b] = ref;
-                if ([r, g, b].every(n => typeof n === 'number' && n >= 0 && n <= 255))
-                {
-                    return _for === 'fg' ? `\u001b[38;2;${r};${g};${b}m` : `\u001b[48;2;${r};${g};${b}m`;
-                }
-
-                else
-                {
-                    throw new Error('RGB values must be numbers between 0 and 255');
-                }
-            }
-
+            // by array
             else
             {
-                throw new Error('ref must be a non-empty string or array[n,n,n]');
+                // by array : rgb
+                {
+                    // t_rgb -> r, g, b
+                    const [r, g, b] = ref;
+
+                    // r, g, b -> string
+                    if ([r, g, b].every(n => typeof n === 'number' && n >= 0 && n <= 255))
+                    return _for === 'fg' ? `\u001b[38;2;${r};${g};${b}m` : `\u001b[48;2;${r};${g};${b}m`;
+                }
+
+                // invalid array syntax
+                throw new Error('Invalid array color syntax');
             }
         }
     };
